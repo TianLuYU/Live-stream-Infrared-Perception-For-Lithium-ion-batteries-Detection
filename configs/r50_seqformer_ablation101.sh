5861c731d1633b395007d7be0c9a26ec8b82053e
TianLuYU <127036102+TianLuYU@users.noreply.github.com>
2025-03-19 11:24:07 +0800

    Add files via upload

---
+++ configs/r50_seqformer_ablation101.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -x
+
+python3 -u main.py \
+    --dataset_file YoutubeVIS \
+    --epochs 100 \
+    --lr 2e-4 \
+    --lr_drop 2 10\
+    --batch_size 2 \
+    --num_workers 0 \
+    --coco_path ../coco \
+    --ytvis_path /content/drive/MyDrive/Tian/Seqformer_me/ytvis_32view \
+    --num_queries 300 \
+    --num_frames 1 \
+    --with_box_refine \
+    --masks \
+    --rel_coord \
+    --backbone resnet101 \
+    --pretrain_weights /content/drive/MyDrive/Tian/Seqformer_me/weights/r101_weight.pth \
+    --output_dir r101_1.14_100 \
+
